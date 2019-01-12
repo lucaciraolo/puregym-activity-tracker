aef25c95d4ca07c1715a0e21fa2d7f39d5441cf5
Luca Ciraolo <lucaciraolo@gmail.com>
2019-01-12 20:53:14 +0000

    added dotenv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
 import pupeteer from "puppeteer";
+
+dotenv.config();
 
 async function run() {
     const browser = await pupeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto('https://github')
+    // await page.goto('https://github')
 }
 
 run();
